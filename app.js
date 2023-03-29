fc9912d0cfc6c6d6365300b70c15d2870597e741
ulrikaha <98079099+ulrikaha@users.noreply.github.com>
2023-03-29 11:11:56 +0200

    second commit

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-//Require dotenv and config
-require('dotenv').config();
-
-// async errors
-
 //Require express and initialize app
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
-const notFoundMiddlewear = require('./middleware/not-found');
-const errorMiddlewear = require('./middleware/error-handler');
+
 
 //middlewear
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //routes
 app.get('/', (req, res) => {
-    res.send('Store Api');
+    res.send('Store API');
 });
     
+
+module.exports = app;
+
+
+
---
+++ server.js
@@ -0,0 +1,13 @@
+const app = require('./app');
+const mongoose = require('mongoose');
+const cors = require('cors');
+cors();
+require('dotenv').config();
+
+const PORT = process.env.PORT || 9999;
+
+app.listen(PORT, () => console.log('Server is running on port' + PORT));
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err.message));
