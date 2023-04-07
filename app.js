3275dd227a4cf8b20d584d23618d447e96ea5622
ulrikaha <98079099+ulrikaha@users.noreply.github.com>
2023-04-07 14:50:21 +0200

    update n delete user

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ app.use(express.json());
 
 //controllers
 app.use('/api/products', require('./controllers/productController'));
-
 app.use('/api/users', require('./controllers/userController'));
+//app.use('/api/orders', require('./controllers/orderController'));
 
 
 
--- models/userModel.js
+++ models/userModel.js
@@ -98,6 +98,42 @@ const loginUser = async (req, res) => {
         };
 
 
+        const updateUser = async (req, res) => {
+            try {
+                const data = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+                if (!data) {
+                    res.status(404).json({
+                        message: 'Could not find this user'
+                    });
+                    return;
+                }
+                res.status(200).json(data);
+            } catch (err) {
+                res.status(500).json({
+                    message: 'Something went wrong when updating this user!',
+                    err: err.message
+                });
+            }
+        };
+        
+        const deleteUser = async (req, res) => {
+            try {
+                const data = await User.findByIdAndDelete(req.params.id);
+                if (!data) {
+                    res.status(404).json({
+                        message: 'Could not find this user'
+                    });
+                    return;
+                }
+                res.status(200).json(data);
+            } catch (err) {
+                res.status(500).json({
+                    message: 'Something went wrong when deleting this user!',
+                    err: err.message
+                });
+            }
+        };
+
 
 
 
@@ -107,5 +143,8 @@ module.exports = {
   registerNewUser, 
     loginUser,
     getUserData,
-    getAllUsers
+    getAllUsers,
+    updateUser,
+    deleteUser
+
 }
